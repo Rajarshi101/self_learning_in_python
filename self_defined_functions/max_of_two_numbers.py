1e6b64c75664c970f80fc85223205f0312951b87
Rajarshi101 <109743720+Rajarshi101@users.noreply.github.com>
2022-10-15 19:53:00 +0530

    Update max_of_two_numbers.py

--- self_defined_functions/max_of_two_numbers.py
+++ self_defined_functions/max_of_two_numbers.py
@@ -2,6 +2,11 @@ def max_of_two(x,y):
   if x>y:
     return x
   return y
+'''
+The above defined function is the basic algorithm.
+The code below is extra, just to present a complete executable code.
+The code below is just to input data and output the result utilising the function defined above.
+'''
 print("Enter two numbers:--")
 num1=int(input())
 num2=int(input())
